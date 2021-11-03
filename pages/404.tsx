4a857de21c6f0c739028b937ba4b7f2102b9a43a
etotasam <spanish2007@gmail.com>
2021-11-03 23:21:15 +0900

    404ページの修正

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,24 +1,31 @@
 import React from "react";
-import { useRouter } from "next/router";
+import Link from "next/link";
 
 const NotFound = () => {
-  const router = useRouter();
-
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      router.push(`/`);
-    }, 3000);
-    return () => {
-      clearTimeout(timer);
-    };
-  });
-
   return (
-    <div>
-      <h1>存在しないページです</h1>
-      <p>Homeへ移動します</p>
+    <div className={`t-main-height flex justify-center items-center`}>
+      <div className={`text-center`}>
+        <h1 className={`text-5xl mb-5`}>404</h1>
+        <p className={`mb-2`}>ご指定のページが見つかりませんでした</p>
+        <p>お探しのページは削除されたか、URLが変更された可能性があります</p>
+        <Link href={`/`}>
+          <a
+            className={`inline-block text-white bg-green-400 rounded-full py-2 px-10 mt-5`}
+          >
+            Homeへ
+          </a>
+        </Link>
+      </div>
     </div>
   );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      layout: `plain`,
+    },
+  };
 };
 
 export default NotFound;
